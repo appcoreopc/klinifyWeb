221548254372b933011582764feeff2ee54edc72
jeremy woo <jeremy.woo@akqa.com>
2018-09-26 17:03:28 +1200

    added cropper feature.

--- src/components/user/UserContent.tsx
+++ src/components/user/UserContent.tsx
@@ -3,16 +3,30 @@
   import { connect } from 'react-redux';
   import { PHOTO_UPLOAD, PHOTO_UPLOAD_SUCCEEDED } from '../../constants';
   import Dropzone from 'react-dropzone';
+  import Cropper from 'react-cropper';
+
 
   class MainContent extends React.Component<any, any> {
     
+    src : string = '';
+    cropper : any;
+
     constructor(props) { 
       super(props);
       this.state = {
         loading : false,
-        files : []      
+        files : [],
+        src : this.src,
+        cropResult: null
       };
+
+
+      this.cropImage = this.cropImage.bind(this);
+      this.onChange = this.onChange.bind(this);
+      this.useDefaultImage = this.useDefaultImage.bind(this);
+      this.src = 'images/child.jpg';
     }
+    
     
     uploadPhoto = async () => {        
       let result = await this.executeFileUpload();
@@ -22,7 +36,35 @@
     async executeFileUpload() : Promise<string> { 
       return Promise.resolve("http://lorempixel.com/800/100/cats/");
     }
-    
+
+    onChange(e) {
+      e.preventDefault();
+      let files;
+      if (e.dataTransfer) {
+        files = e.dataTransfer.files;
+      } else if (e.target) {
+        files = e.target.files;
+      }
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.setState({ src: reader.result });
+      };
+      reader.readAsDataURL(files[0]);
+    }
+  
+    cropImage() {
+      if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
+        return;
+      }
+      this.setState({
+        cropResult: this.cropper.getCroppedCanvas().toDataURL(),
+      });
+    }
+  
+    useDefaultImage() {
+      this.setState({ src : this.src });
+    }
+   
     prepareFileToUpload = (files) => {        
       console.log(files);     
       this.setState({
@@ -43,62 +85,52 @@
       return (   
         
         <div className="center-screen">
-        <div>
-        <div>
-        
-        <div>
-        
-        <div>
-        
-        </div>
-        <div>
-        <h3> <small>  Upload photos  - select a photo, crop and upload it.</small></h3>
-        </div>
-        
-        <div>          
-        <div>
-  
-        </div>
-        </div>
-        
-        </div>      
-        
-        <div>
-        <div>                
-        </div>
-        
-        <div>
-        <div> 
-        
-        <Dropzone accept="image/jpeg, image/png" onDrop={(files) => {        
-          this.prepareFileToUpload(files);        
-        }}>
-        <p>Please drop your image file or click to select image to upload.</p>
-        </Dropzone>
-
-        <aside>
-        <h2> Upload my file </h2>
-        <ul>
-        {
-          this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-        }
-        </ul>
-        </aside>             
               
-        <Button type="primary" onClick={() => {
-          this.uploadPhoto();
-        }} >Upload photo </Button>
+            <div>
+            <h3> <small>  Upload photos  - select a photo, crop and upload it.</small></h3>
+            </div>
+                                
+          <div> 
+            
+              <Dropzone accept="image/jpeg, image/png" onDrop={(files) => {        
+                this.prepareFileToUpload(files);        
+              }}>
+              <p>Please drop your image file or click to select image to upload.</p>
+              </Dropzone>
+
+              <aside>
+              <h2> Upload my file </h2>
+              <ul>
+              {
+                this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+              </ul>
+              </aside>             
+                    
+              <Button type="primary" onClick={() => {
+                this.uploadPhoto();
+              }} >Upload photo </Button>
+            
+
+          </div>
+
+            
+        <div style={{ width: '100%' }}>
+          <input type="file" onChange={this.onChange} />
+          <button onClick={this.useDefaultImage}>Use default img</button>
+          <br />
+          <br />
+          <Cropper
+            style={{ height: 400, width: '100%' }}
+            aspectRatio={16 / 9}
+            preview=".img-preview"
+            guides={false}
+            src={this.state.src}
+            ref={cropper => { this.cropper = cropper; }}
+          />
+        </div>
+
         
-        </div>
-                
-        </div>
-        </div>      
-        
-        <div>
-        </div>
-        
-        </div>
-        </div>
         </div>
         
       );
