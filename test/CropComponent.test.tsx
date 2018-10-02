e70ceeb5aeb65387ed9a071e7a66be956d057700
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-10-02 21:34:52 +1300

    adeded jester and jester-ts testing toolchain

---
+++ test/CropComponent.test.tsx
@@ -0,0 +1,14 @@
+
+import * as React from "react";
+import Link from '../src/components/user/Link';
+import * as renderer from 'react-test-renderer';
+
+test('Link changes the class when hovered', () => {
+  
+  const component = renderer.create(
+    <Link/>,
+  );
+ 
+ 
+});
+
